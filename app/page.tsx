4a142cad23049f3527c8642f836241d984d06083
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-02-09 21:24:48 +0200

    Updated `package.json` config and `VideoCard.tsx`

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import VideoCard from "@/components/VideoCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <div className="flex flex-wrap">
+        <VideoCard />
+        <VideoCard />
+      </div>
     </main>
   )
 }
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,5 +1,7 @@
 export default function VideoCard() {
   return (
-    <div></div>
+    <div className="w-full m-2 border-2 border-red-500 sm:max-w-sm">
+
+    </div>
   )
 }
