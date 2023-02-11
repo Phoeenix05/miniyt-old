c950dd2adb1a0dd9f9231a98bf7122d3abbd4955
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-02-11 17:23:00 +0200

    Added Regex expression to extract data from `html`

--- youtube-scraper/src/main.rs
+++ youtube-scraper/src/main.rs
@@ -1,37 +1,21 @@
+use regex::Regex;
 use reqwest::{
     blocking::Client,
     header::{HeaderMap, HeaderValue, ACCEPT_ENCODING, ACCEPT_LANGUAGE, USER_AGENT},
 };
 
-// async fn fetch() -> Result<String, reqwest::Error> {}
 fn fetch() -> Result<String, reqwest::Error> {
-    // let client = reqwest::Client::new();
-    // let results = client
-    //     .get("https://www.youtube.com/results?search_query=brian+x+stephanie+swoard+art+online")
-    //     .header("user-agent", "miniyt-scraper")
-    //     .send().await?;
-
-    // reqwest::blocking::get(
-    //     // "https://www.youtube.com/@BrianxStephanie/playlists?view=1&sort=dd&shelf_id=0",
-    //     "https://www.youtube.com/results?search_query=brian+x+stephanie+swoard+art+online",
-    // )?
-    // .text()
-
-    // let client = reqwest::blocking::Client::new();
     let mut headers = HeaderMap::new();
+    // User-Agent miniyt-scraper or miniyt/scraper
     headers.insert(USER_AGENT, HeaderValue::from_static("miniyt/scraper"));
     headers.insert(ACCEPT_LANGUAGE, HeaderValue::from_static("en"));
     headers.insert(ACCEPT_ENCODING, HeaderValue::from_static("utf-8"));
-    
+
     let client = Client::new();
 
     let results = client
         .get("https://www.youtube.com/results?search_query=brian+x+stephanie+swoard+art+online")
         .headers(headers)
-        // .header("User-Agent", "miniyt-scraper")
-        // .header("Accept-Language", "en")
-        // // .header("Accept-Encoding", "gzip")
-        // .header("Accept-Encoding", "utf-8")
         .send()?;
 
     results.text()
@@ -40,7 +24,7 @@ fn fetch() -> Result<String, reqwest::Error> {
 fn parse_data(data: &String) -> String {
     // let start = "var ytInitialData = ";
     // let end = ";</script>";
-    
+
     // data.split(&start);
 
     // let start_bytes = data.find("var ytInitialData = ").unwrap_or(0);
@@ -49,23 +33,28 @@ fn parse_data(data: &String) -> String {
     // let result = &data[start_bytes..end_bytes];
     // result.to_string()
 
-    "".to_string()
+    // let re = Regex::new("(?s)var ytInitialData = .*;</script>").unwrap();
+    // let re = Regex::new("(?<=var ytInitialData) = (.*)(?=;</script>)").unwrap();
+    // let re = Regex::new("var ytInitialData = (?s)(.*);</script>").unwrap();
+    // let re = Regex::new(
+    //     "/var\\sytInitialData\\s=\\s(.*);<\\/script>/
+    // ",
+    // )
+    // .unwrap();
+    // let result = re.split(data);
+
+    let re = Regex::new(r#"var ytInitialData = (.+?);</script>"#).unwrap();
+    let captures = re.captures(data).unwrap();
+    let result = &captures[1].to_string();
+    // &captures[1].to_string()
 }
 
-// #[tokio::main]
-// async fn main() -> Result<(), reqwest::Error> {}
 fn main() -> Result<(), reqwest::Error> {
-    // println!("Hello, world!");
-    // let result =
     let result = match fetch() {
-        // Ok(res) => {
-        //     // let json = serde_json::from_str(&res);
-        //     println!("{:#?}", res)
-        // }
         Ok(res) => {
             let data = parse_data(&res);
             println!("{:#?}", data)
-        },
+        }
         Err(err) => eprintln!("{}", err),
     };
 
