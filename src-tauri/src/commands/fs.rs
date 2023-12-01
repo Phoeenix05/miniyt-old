b1f7a81595c0dfefa40ffe5ebdd64dab200ca38e
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-12-01 16:26:14 +0200

    chore: saving

---
+++ src-tauri/src/commands/fs.rs
@@ -0,0 +1,5 @@
+#[tauri::command]
+pub fn app_data_dir_exists() -> () {}
+
+#[tauri::command]
+pub fn create_app_data_dir() -> () {}
--- src/components/video/video-card.tsx
+++ src/components/video/video-card.tsx
@@ -1,32 +1,33 @@
-import { A } from "@solidjs/router"
-import { SearchResult } from "../../util/invidious/search"
-import { AuthorCard } from "../author-card"
+import { A } from "@solidjs/router";
+import { SearchResult } from "../../util/invidious/search";
+import { AuthorCard } from "../author-card";
 
 export interface Props {
-    data: SearchResult,
+  data: SearchResult;
 }
 
-export const VideoCard = (props: Props) => {    
-    return (
-        <div>
-            <A href={`/video/${props.data.videoId}`}>
-                <img src={props.data.videoThumbnails?.at(4)?.url} class="m-0" />
-                <h2>{props.data.title}</h2>
-            </A>
-            <AuthorCard 
-                author={props.data.author} 
-                authorId={props.data.authorId}
-                authorUrl={props.data.authorUrl}
-            />
-        </div>
-    )
-}
+export const VideoCard = (props: Props) => {
+  return (
+    <div>
+      <A href={`/video/${props.data.videoId}`}>
+        <img src={props.data.videoThumbnails?.at(4)?.url} class="m-0" />
+        <h2>{props.data.title}</h2>
+      </A>
+      <AuthorCard
+        author={props.data.author}
+        authorId={props.data.authorId}
+        authorUrl={props.data.authorUrl}
+      />
+    </div>
+  );
+};
 
-export const HorizontalVideoCard = (props: Props) => {    
-    return (
-        <A href={`/video/${props.data.videoId}`}>
-            <img src={props.data.videoThumbnails?.at(5)?.url} class="m-0" />
-            <h2>{props.data.title}</h2>
-        </A>
-    )
-}
+export const HorizontalVideoCard = (props: Props) => {
+  return (
+    <A href={`/video/${props.data.videoId}`}>
+      <img src={props.data.videoThumbnails?.at(5)?.url} class="m-0" />
+      <h2>{props.data.title}</h2>
+    </A>
+  );
+};
+
